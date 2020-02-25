36d013632e8d608e0ed7e2f616e7e9be395a2ef2
Ryan Addessi (raddessi) <raddessi@laptop-thorium.raddessi.net>
2020-02-24 22:04:48 -0700

    Updated dict .keys() and .values() indexed calls for py3

--- salt/modules/postgres.py
+++ salt/modules/postgres.py
@@ -769,7 +769,7 @@ def tablespace_alter(name, user=None, host=None, port=None, maintenance_db=None,
                        name, new_owner))
     if set_option:
         queries.append('ALTER TABLESPACE "{0}" SET ({1} = {2})'.format(
-                       name, set_option.keys()[0], set_option.values()[0]))
+                       name, list(set_option.keys())[0], list(set_option.values())[0]))
     if reset_option:
         queries.append('ALTER TABLESPACE "{0}" RESET ({1})'.format(
                        name, reset_option))
